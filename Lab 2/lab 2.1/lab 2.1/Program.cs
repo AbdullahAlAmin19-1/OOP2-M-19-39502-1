4e9188c2a215a859912a4d918f67e4c15cdb906f
AbdullahAlAmin19-1 <abdullahalamin1211@gmail.com>
2021-02-08 09:42:16 +0600

    lab 2

---
+++ Lab 2/lab 2.1/lab 2.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.Write("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Even Numbers: ");
+            for (i = 1; i <= n; i++)
+            { 
+                if( i%2 == 0 ) { Console.Write(i + "\n"); } 
+            }
+        }
+    }
+}
---
+++ Lab 2/lab2.4/lab2.4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, Sum_Of_Even=0, Sum_Of_Odd=0;
+            Console.Write("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Sum_Of_Even = Sum_Of_Even + i;
+                }
+
+                else if (i % 2 != 0)
+                {
+                    Sum_Of_Odd = Sum_Of_Odd + i;
+                }
+            }
+                Console.Write("The Sum of Even Numbers: ");
+                Console.WriteLine(Sum_Of_Even);
+                Console.Write("The Sum of Odd Numbers: ");
+                Console.WriteLine(Sum_Of_Odd);
+            
+        }
+    }
+}
