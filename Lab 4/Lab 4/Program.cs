bd78a6ea98a850d80ed5f7d42d863d295418a8f8
AbdullahAlAmin19-1 <abdullahalamin1211@gmail.com>
2021-02-22 10:59:05 +0600

    lab 4

---
+++ Lab 4/Lab 4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Savings a1 = new Savings("Abdullah", "1234", 5000);
+            Savings a2 = new Savings("Al-Amin", "4321", 4000);
+            a1.Withdraw(500);
+            a1.Deposit(1000);
+            a1.Transfer(1000, a2);
+
+        }
+    }
+}
---
+++ Lab 4/Lab 4/Savings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    class Savings : Account
+    {
+        int minBalance = 430;
+        public Savings(){}
+        public Savings(string accName, string accid, int balance):base (accName, accid, balance)
+        {
+            //this.AccName = accName;
+            //this.Accid = accid;
+            //this.Balance = balance;
+        }
+
+        new public void Withdraw(int amount)
+        {
+            if (amount > minBalance)
+                base.Withdraw(amount);
+            else
+                Console.WriteLine("Insaficient Balance.");
+        }
+
+        new public void Deposit(int amount){ base.Deposit(amount); }
+
+        new public void Transfer (int amount, Account acc)
+        {
+            if (amount > minBalance)
+                base.Transfer (amount,acc);
+            else
+                Console.WriteLine("Insaficient Balance.");
+        }
+    }
+}
